a6fbad7fb0e0b3298a97d64100a88b91fd2b587d
Beto Toro <beto.toro@zemoga.com>
2022-06-28 09:20:26 -0500

    Initial Commit

---
+++ unitConvertions/unitConvertionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unitConvertionsApp.swift
+//  unitConvertions
+//
+//  Created by Beto Toro on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct unitConvertionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
